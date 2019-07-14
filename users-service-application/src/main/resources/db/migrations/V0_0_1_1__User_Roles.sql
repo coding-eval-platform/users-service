2727e7504bda471e1a1c1d3be703f8e9ea68aabd
Juan Marcos Bellini <juan.bellini@wolox.com.ar>
2019-07-14 19:47:26 -0300

    Fix user roles migration
    - Remove the 64 limit

--- users-service-application/src/main/resources/db/migrations/V0_0_1_1__User_Roles.sql
+++ users-service-application/src/main/resources/db/migrations/V0_0_1_1__User_Roles.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_roles
 (
     user_id BIGSERIAL PRIMARY KEY NOT NULL,
-    role    VARCHAR(64)           NOT NULL,
+    role    VARCHAR               NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
