e09723de9a2b74b45eadf23685e7359d83b90247
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-07-15 03:58:19 -0300

    Add the token model
    - Create java model, together with migration script and ORM mapping

---
+++ users-service-core/users-service-models/src/main/java/ar/edu/itba/cep/users_service/models/AuthToken.java
@@ -0,0 +1,111 @@
+package ar.edu.itba.cep.users_service.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.util.Assert;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Represents a authentication/authorization token belonging to a {@link User}.
+ */
+@Getter
+@ToString(doNotUseGetters = true)
+@EqualsAndHashCode(of = "id", doNotUseGetters = true)
+public class AuthToken {
+
+    /**
+     * The token's id.
+     */
+    private final UUID id;
+    /**
+     * The {@link User} owning the token.
+     */
+    private final User user;
+    /**
+     * The {@link Role}s assigned to the token
+     * (i.e what the {@link User} is allowed to do when presenting this token).
+     */
+    private final Set<Role> rolesAssigned;
+    /**
+     * The {@link Instant} in which this token is created.
+     */
+    private final Instant createdAt;
+    /**
+     * A flag indicating whether this token is valid.
+     */
+    private boolean valid;
+
+
+    /**
+     * Default constructor.
+     */
+    /* package */ AuthToken() {
+        // Initialize final fields with default values.
+        this.id = null;
+        this.user = null;
+        this.rolesAssigned = null;
+        this.createdAt = null;
+    }
+
+    /**
+     * @param user          The {@link User} owning the token.
+     * @param rolesAssigned The {@link Role}s assigned to the token
+     *                      (i.e what the {@link User} is allowed to do when presenting this token).
+     *                      Note that these {@link Role}s don't have to be the same
+     *                      as the given {@code user} {@link Role}s
+     *                      (for example, the {@code user} might have been granted a {@link Role} using only this
+     *                      token).
+     * @throws IllegalArgumentException In case any value is not a valid one.
+     */
+    public AuthToken(final User user, final Set<Role> rolesAssigned) throws IllegalArgumentException {
+        assertUser(user);
+        assertRoles(rolesAssigned);
+        this.id = null;
+        this.user = user;
+        this.rolesAssigned = rolesAssigned;
+        this.createdAt = Instant.now();
+        this.valid = true;
+    }
+
+
+    /**
+     * @return The {@link Role}s assigned to the token
+     * (i.e what the {@link User} is allowed to do when presenting this token).
+     */
+    public Set<Role> getRolesAssigned() {
+        return Collections.unmodifiableSet(rolesAssigned);
+    }
+
+
+    // ================================
+    // Assertions
+    // ================================
+
+    /**
+     * Asserts that the given {@code user} is valid.
+     *
+     * @param user The {@link User} to be checked.
+     * @throws IllegalArgumentException In case the user is not a valid one.
+     */
+    private static void assertUser(final User user) throws IllegalArgumentException {
+        Assert.notNull(user, "The user must not be null");
+
+    }
+
+    /**
+     * Asserts that the given {@code roles} {@link Set} is valid.
+     *
+     * @param roles The {@link Role}s {@link Set} to be checked.
+     * @throws IllegalArgumentException In case the {@link Role}s {@link Set} is not a valid one.
+     */
+    private static void assertRoles(final Set<Role> roles) throws IllegalArgumentException {
+        Assert.notNull(roles, "The roles Set must not be null");
+        Assert.isTrue(roles.stream().noneMatch(Objects::isNull), "The roles set contains nulls");
+    }
+}
