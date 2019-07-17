3f53461bd60ca9906910401f3d969acec444430f
Juan Marcos Bellini <juan.bellini@wolox.com.ar>
2019-07-17 00:58:37 -0300

    Make the SpringDataAuthTokenRepository fetch assigned roles
    - Create a custom query for the findByUser method

--- users-service-adapters/users-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/users_service/spring_data/interfaces/SpringDataAuthTokenRepository.java
+++ users-service-adapters/users-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/users_service/spring_data/interfaces/SpringDataAuthTokenRepository.java
@@ -2,7 +2,9 @@ package ar.edu.itba.cep.users_service.spring_data.interfaces;
 
 import ar.edu.itba.cep.users_service.models.AuthToken;
 import ar.edu.itba.cep.users_service.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,5 +20,10 @@ public interface SpringDataAuthTokenRepository extends CrudRepository<AuthToken,
      * @param user The {@link User} owning the returned {@link AuthToken}s.
      * @return A {@link List} containing al the given {@link User}'s {@link AuthToken}s.
      */
-    List<AuthToken> findByUser(final User user);
+    @Query(value = "SELECT DISTINCT at " +
+            "       FROM AuthToken at " +
+            "           LEFT JOIN FETCH at.rolesAssigned" +
+            "       WHERE at.user = :user" +
+            "       ORDER BY at.createdAt")
+    List<AuthToken> findByUser(@Param("user") final User user);
 }
