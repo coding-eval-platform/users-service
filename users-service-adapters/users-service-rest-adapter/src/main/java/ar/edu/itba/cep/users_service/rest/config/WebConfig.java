9e88d427f6ed29d1c9f8fc45d783e1cf50e46f36
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-08-24 17:51:02 -0300

    Add the ForwardedHeaderFilter
    - This filter allows the application to process the Forwarded and X-Forwarded-* headers
    - Closes #21

--- users-service-adapters/users-service-rest-adapter/src/main/java/ar/edu/itba/cep/users_service/rest/config/WebConfig.java
+++ users-service-adapters/users-service-rest-adapter/src/main/java/ar/edu/itba/cep/users_service/rest/config/WebConfig.java
@@ -4,9 +4,12 @@ import com.bellotapps.webapps_commons.config.EnableJerseyApplication;
 import com.bellotapps.webapps_commons.exceptions.CustomConstraintViolationException;
 import com.bellotapps.webapps_commons.validation.jersey.ConstraintViolationExceptionCreator;
 import com.bellotapps.webapps_commons.validation.jersey.EnableJerseyValidation;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 /**
  * Configuration class in charge of configuring web concerns.
@@ -24,8 +27,27 @@ import org.springframework.context.annotation.Configuration;
 @EnableJerseyValidation
 public class WebConfig {
 
+    /**
+     * A bean of a {@link CustomConstraintViolationException}.
+     *
+     * @return A {@link CustomConstraintViolationException} bean.
+     */
     @Bean
     public ConstraintViolationExceptionCreator constraintViolationExceptionCreator() {
         return CustomConstraintViolationException::new;
+    }
+
+    /**
+     * Creates a {@link FilterRegistrationBean} for a {@link ForwardedHeaderFilter}, in order to process the
+     * Forwarded and X-Forwarded-* headers.
+     *
+     * @return The {@link FilterRegistrationBean}.
+     */
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        final var bean = new FilterRegistrationBean<ForwardedHeaderFilter>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE + 10);
+        return bean;
     }
 }
