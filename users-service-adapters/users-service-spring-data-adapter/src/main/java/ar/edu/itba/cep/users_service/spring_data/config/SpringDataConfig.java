0ad154a21fbf6217e6f43e558adaa24eb3bad7d6
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-11-07 03:01:10 -0300

    Mini fix in the SpringDataConfig class
    - Remove unnedded annotation

--- users-service-adapters/users-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/users_service/spring_data/config/SpringDataConfig.java
+++ users-service-adapters/users-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/users_service/spring_data/config/SpringDataConfig.java
@@ -1,6 +1,5 @@
 package ar.edu.itba.cep.users_service.spring_data.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,9 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 @EnableJpaRepositories(basePackages = {
         "ar.edu.itba.cep.users_service.spring_data.interfaces"
-})
-@EntityScan(basePackages = {
-        "ar.edu.itba.cep.users_service.models"
 })
 public class SpringDataConfig {
 }
